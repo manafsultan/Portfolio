5b74499797c2d8b386f7c1b51173eda14c01d8a6
codeologie <manafsultan90@gmail.com>
2020-09-20 18:36:36 +0200

    change the contact link service to ajax

--- JS/contact.js
+++ JS/contact.js
@@ -1,24 +1,22 @@
 window.addEventListener("DOMContentLoaded", function() {
+// get the form elements defined in the HTML
+  var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
 
-  // get the form elements defined in your form HTML above
-  
-  var form = document.getElementById("contact");
-  var name = document.getElementById("inputName");
-  var email = document.getElementById("inputEmail");
+// Success and Error functions for after the form is submitted
 
-  // Success and Error functions for after the form is submitted
-  
-  function success() {
-    form.reset();
-    button.style = "display: none ";
-    status.innerHTML = "Thanks!";
-  }
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks!";
+    }
 
-  function error() {
-    status.innerHTML = "Oops! There was a problem.";
-  }
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
 
-  // handle the form submission event
+// handle the form submission event
 
   form.addEventListener("submit", function(ev) {
     ev.preventDefault();
