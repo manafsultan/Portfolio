35a7ba7d7423b58b2130056ab3a25106c392420f
codeologie <manafsultan90@gmail.com>
2020-10-10 22:33:09 +0200

    adding read more/less in services and changing photo for astra

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function(){
     
     setTimeout(() =>{
         document.querySelector(".preloader").style.display="none";
-    },1000)
+    },1000);
 });
 
 // Portfolio Item Filter
@@ -97,5 +97,39 @@ function asideSectionTogglerBtn(){
 
     for(let i=0; i<totalSection; i++){
         allSection[i].classList.toggle("open");
+    }
+}
+
+// Read more/Read less
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.style.display = "none"; 
+        moreText.style.display = "inline";
+    }
+}
+
+function monFunction() {
+    var poin = document.getElementById("poin");
+    var plusText = document.getElementById("plus");
+    var botnText = document.getElementById("monBtn");
+
+    if (poin.style.display === "none") {
+        poin.style.display = "inline";
+        botnText.innerHTML = "Read more"; 
+        plusText.style.display = "none";
+    } else {
+        poin.style.display = "none";
+        botnText.style.display = "none"; 
+        plusText.style.display = "inline";
     }
 }
